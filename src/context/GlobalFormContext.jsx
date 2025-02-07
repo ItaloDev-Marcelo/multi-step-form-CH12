adb0374090a8f8d4ee927d02fa61c5bf5a802ed9
Marcelo Prado <Devfrontend2028study@gmail.com>
2025-02-07 17:52:15 -0300

    Atualizar o GlobalFormContext.jsx

--- src/context/GlobalFormContext.jsx
+++ src/context/GlobalFormContext.jsx
@@ -7,7 +7,7 @@ export default function MultFormProvider({children})  {
 
     const [dataForm0, setDataForm0] = useState(null)
     const [dataForm1, setDataForm1] = useState(null)
-    console.log(dataForm0, dataForm1)
+    alert(dataForm0, dataForm1)
  
     return <GlobalFormContext.Provider value={{setDataForm0, setDataForm1}}>{children}</GlobalFormContext.Provider>
 }
