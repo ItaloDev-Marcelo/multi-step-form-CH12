ed64d85fa4578cd5b03bf3fa2a1c5f9a093e7fda
Italo Marcelo <devfrontend2028study@gmail.com>
2025-02-11 16:57:12 -0300

    update

--- src/components/form-finish/index.jsx
+++ src/components/form-finish/index.jsx
@@ -12,6 +12,7 @@ export default function Result() {
   let FormCheckList = formMultStepData[2]?.checkList || [];
   let mix = [...FormCheckList]
   let mix2 = [...FormSelected]
+  let mix3 = [...FormCheckList, ...FormSelected]
   
   function convertData(formStep) {
       let FormData = formStep && formStep.map((item) => {
@@ -24,6 +25,7 @@ export default function Result() {
 
   let data01 =  convertData(mix);
   let data02 =  convertData(mix2);
+  let data03 =  convertData(mix3);
 
   console.log(data01)
 
@@ -35,6 +37,16 @@ export default function Result() {
         </div>
      )
   }) 
+
+
+
+const re = data03.map(item => Number(item.price)).
+reduce((sum, num) => {
+    return sum + num
+},0)
+
+console.log(re)
+
 
   const item2 = data02.map(item => {
     return (
@@ -72,6 +84,10 @@ export default function Result() {
        {item2}
        {item}
        </article>
+        <div className="total">
+          <h5>total per {data03[2] == 'mo' ? 'month' : 'year'} </h5>
+          <p>${re}/{data03[2] == 'mo' ? 'mo' : 'yr'}</p>
+        </div>
       </section>
       <div className="Navbar">
      <button onClick={() => navigate('/form-step1/form-step2/form-step3')} className="btn-Dark-Blue">Go back</button> 
